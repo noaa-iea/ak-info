621fe59eeef2c7ce63ec59ad4ec661f12fd607df
Ben Best <ben@ecoquants.com>
2019-08-29 13:07:42 -0700

    redo_modals, mathjax=NULL

--- render_site.R
+++ render_site.R
@@ -3,7 +3,12 @@ library(glue)
 library(fs)
 library(rmarkdown)
 
-d <- read_csv("svg/ebs_links.csv")
+# parameters
+csv         <- "svg/ebs_links.csv"
+redo_modals <- F
+
+# read in links for svg
+d <- read_csv(csv)
 
 # create/render modals by iterating over svg links in csv
 for (i in 1:nrow(d)){
@@ -14,9 +19,10 @@ for (i in 1:nrow(d)){
   # create Rmd, if doesn't exist
   if (!file.exists(rmd)) file.create(rmd)
   
-  # render Rmd to html, if Rmd newer
-  if (file_info(rmd)$modification_time > file_info(htm)$modification_time){
-    render(rmd, html_document(self_contained=F, lib_dir = "modal_libs", mathjax = "local"))
+  # render Rmd to html, if Rmd newer or redoing
+  rmd_newer <- file_info(rmd)$modification_time > file_info(htm)$modification_time
+  if (rmd_newer | redo_modals){
+    render(rmd, html_document(self_contained=F, lib_dir = "modal_libs", mathjax = NULL))
   }
 }
 
